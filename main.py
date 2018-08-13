8b613b86bf82f73645cee07c32e9561bca32ac86
iusami <usami@angelplayingcards.com>
2018-08-13 15:04:48 +0900

    Get config from config.json

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from measure_and_save import measure_and_save
 
 
 def main():
-    color_list = ["red","blue","green","lgreen","brown","black","orange","yellow","violet","pink","gray","lred"]
-    savefile_flag = 0
-    SAVEFILENAME = "thicknessdata.json"
+    file = open("config.json","r")
+    config = json.load(file)
+    color_list = config["list"]
+    SAVEFILENAME = config["savefile"]
     while(True):
         function = measure_and_save(SAVEFILENAME,"champ_and_lower.json", "session.json",color_list)
         end_flag = function.save_to_json()
         if end_flag ==1 :
             break
     print("Finish!")
+    file.close()
 
 
 
